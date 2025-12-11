ca1338ca48049860efe4d152391711465eb7734d
tomvictor303 <tomvictor303@gmail.com>
2025-12-10 19:11:24 -0500

    fix field name

--- controllers/refineScrapedController.js
+++ controllers/refineScrapedController.js
@@ -21,7 +21,7 @@ async function getActivePages(hotelUuid) {
 async function saveMarkdown(id, markdown) {
   const query = `
     UPDATE ${HOTEL_PAGE_DATA_TABLE}
-    SET llm_output = ?, updated_at = CURRENT_TIMESTAMP
+    SET markdown = ?, updated_at = CURRENT_TIMESTAMP
     WHERE id = ?
   `;
   try {
