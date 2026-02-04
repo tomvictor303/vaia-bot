346d6c1832dbb4de74f7586293d900440459f879
tomvictor303 <tomvictor303@gmail.com>
2026-02-04 16:01:17 -0500

    chore

--- middleware/constants.js
+++ middleware/constants.js
@@ -23,7 +23,7 @@ export const MD_CAT_FIELDS = [
 	{ name: 'food_beverage', description: 'Food & beverage (Dining, bar, café, and catering services provided; plus cuisine, meals, and drinks served)' },
 	{
 		name: 'guest_rooms',
-		description: 'Guest Rooms - Room type and all details per type including inventory count; renovation status; bed type; sleeps count / occupancy; special features; accessibility, pet, and non-smoking notes; key room amenities.',
+		description: 'Guest Rooms - Room types and all details including inventory count pet types; renovation status; bed type; sleeps count / occupancy; special features; accessibility, pet, and non-smoking notes; key room amenities.',
 		capture_guide: 'For each room type, try to capture all stated details. Do not omit any details.'
 	},
 	{ name: 'guest_services_front_desk', description: 'Guest Services / Front Desk - Bell/porter service, Concierge, Lost & found inquiries, Luggage storage, Wake-up calls' },
